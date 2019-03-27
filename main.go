630dab44e4704673faf0e43a22b0669071dd886d
Conrad Christensen <conrad.christensen@gmail.com>
2019-03-27 13:06:47 +0100

    Added static webapp

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"github.com/rakyll/statik/fs"
+	_ "github.com/smartmachine/crdb-operator-console/webapp"
 	"github.com/smartmachine/crdb-operator/pkg/apis/db/v1alpha1"
 	crdbclient "github.com/smartmachine/crdb-operator/pkg/client/clientset/versioned"
 	informers "github.com/smartmachine/crdb-operator/pkg/client/informers/externalversions"
@@ -11,12 +15,14 @@ import (
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	log "k8s.io/klog"
+	"net/http"
 	"os"
 	"path/filepath"
 )
 
-func main() {
+//go:generate statik -c "Console React Webapp" -Z -f -p webapp -src ../crdb-console/build
 
+func main() {
 	log.InitFlags(nil)
 
 	err := flag.Set("logtostderr", "true")
@@ -68,25 +74,59 @@ func main() {
 				log.Infof("Received heartbeat event for CockroachDB Cluster: %s", newDB.Name)
 				return
 			}
- 		},
+		},
 		DeleteFunc: func(obj interface{}) {
 			mObj := obj.(*v1alpha1.CockroachDB)
 			log.Infof("Deleted CockroachDB Cluster: %s", mObj.Name)
 		},
 	})
 
-	informer.Run(stopper)
+	go informer.Run(stopper)
+
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-			"hasSynced": informer.HasSynced(),
-		})
+
+	r.StaticFS("/console", statikFS)
+	r.GET("/ws", func(c *gin.Context) {
+		wshandler(c.Writer, c.Request)
 	})
 
-	r.Run()
+	err = r.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 
+}
+
+var wsupgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func wshandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := wsupgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Printf("Failed to set websocket upgrade: %+v", err)
+		return
+	}
+
+	for {
+		t, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		err = conn.WriteMessage(t, msg)
+		if err != nil {
+			break
+		}
+	}
 }
 
 func homeDir() string {
